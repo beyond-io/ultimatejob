cbbe51daed81aed6f6e652f3ab70722cc7b28c99
Rom Tamam <rom.tamam@gmail.com>
2020-12-22 19:59:00 +0200

    tests changes

--- API_FB.py
+++ API_FB.py
@@ -3,43 +3,42 @@ from bs4 import BeautifulSoup
 import requests
 
 
-class Manipulation_FB:
-    def extract_jobs_url(self):
-        fb_url = "https://www.facebook.com"
-        jobs_links = []
-        list_jobs_links = []
-        jobs_list = self.find("div", {"class": "_8tk7"})
-        jobs = jobs_list.find_all("a", {"class": "_8sef"})
-        links = [link['href'] for link in jobs]
-        jobs_links = links
-        for link in jobs_links:
-            list_jobs_links.append(fb_url+link)
-            # Need to push to a db
-        print(list_jobs_links)
-        return list_jobs_links
+def extract_jobs_url(soup):
+    fb_url = "https://www.facebook.com"
+    jobs_links = []
+    list_jobs_links = []
+    jobs_list = soup.find("div", {"class": "_8tk7"})
+    jobs = jobs_list.find_all("a", {"class": "_8sef"})
+    links = [link['href'] for link in jobs]
+    jobs_links = links
+    for link in jobs_links:
+        list_jobs_links.append(fb_url+link)
+        # Need to push to a db
+    print(list_jobs_links)
+    return list_jobs_links
 
-    def extract_jobs_title(self):
-        jobs_titles = []
-        list_jobs_titles = []
-        jobs_list = self.find("div", {"class": "_8tk7"})
-        jobs = jobs_list.find_all("div", {"class": "_8sel"})
-        jobs_titles = jobs
-        for title in jobs_titles:
-            list_jobs_titles.append(title.get_text())
-            # Need to push to a db
-        print(list_jobs_titles)
-        return list_jobs_titles
+def extract_jobs_title(soup):
+    jobs_titles = []
+    list_jobs_titles = []
+    jobs_list = soup.find("div", {"class": "_8tk7"})
+    jobs = jobs_list.find_all("div", {"class": "_8sel"})
+    jobs_titles = jobs
+    for title in jobs_titles:
+        list_jobs_titles.append(title.get_text())
+        # Need to push to a db
+    print(list_jobs_titles)
+    return list_jobs_titles
 
-    def create_soup(self):
-        print(self.status_code)
-        soup = BeautifulSoup(self.content, 'html.parser')
-        # print(soup.prettify())
-        return soup
+def create_soup(req):
+    print(req.status_code)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    # print(soup.prettify())
+    return soup
 
-    def create_req():
-        # Load our first page
-        # Need to pull from db
-        search_url = "https://www.facebook.com/careers/jobs/?q="
-        prefrences = "DevOps"
-        r = requests.get(search_url+prefrences)
-        return r
+def create_req():
+    # Load our first page
+    # Need to pull from db
+    search_url = "https://www.facebook.com/careers/jobs/?q="
+    prefrences = "DevOps"
+    req = requests.get(search_url+prefrences)
+    return req
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,8 @@
 # Import and load necessary lib
-from ultimatejobweb.API_FB import Manipulation_FB
+#import sys
+#sys.path.append('vagrant\ultimatejob')
+import API_FB
+import pytest
 
 
 # Test function
@@ -13,7 +16,7 @@ def test_api_urls():
                             'https://www.facebook.com/careers/jobs/2597607417128369/',
                             'https://www.facebook.com/careers/jobs/323705805605018/',
                             'https://www.facebook.com/careers/jobs/536078547072736/']
-    assert Manipulation_FB.extract_jobs_url() == expected_output_urls, "The tests for URLs were successful"
+    assert API_FB.extract_jobs_url() == expected_output_urls, "The tests for URLs were successful"
 
 
 def test_api_titles():
@@ -22,4 +25,4 @@ def test_api_titles():
                               'Internal Audit Manager – Infrastructure', 'Production Engineer',
                               'Production Engineer', 'Return to Work - Production Engineer',
                               'Production Engineer', 'Production Engineer']
-    assert Manipulation_FB.extract_jobs_title() == expected_output_titles, "The tests for URLs were successful"
+    assert API_FB.extract_jobs_title() == expected_output_titles, "The tests for URLs were successful"
