c3e0624d9a9c8824c8c75e94041dcc0cc25848ee
BarBenNaim <73443474+BarBenNaim@users.noreply.github.com>
2020-12-26 18:01:26 +0200

    Update tests_signin_user.py

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -1,6 +1,7 @@
 def test_with_authenticated_client(client, django_user_model):
-	username = "test"
-	password = "test"
-	django_user_model.objects.create_user(username=username, password=password)
-	respons = client.get('/')
-	assert respons.status_code == 200
+        username = "test"
+        password = "test"
+        django_user_model.objects.create_user(username=username, password=password)
+        respons = client.get('/')
+        assert respons.status_code == 200
+	
