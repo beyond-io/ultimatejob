4661f9fcc9a3cadac438705ae961d513b59ae443
BarBenNaim <73443474+BarBenNaim@users.noreply.github.com>
2020-12-28 17:45:29 +0200

    Update tests_signin_user.py

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -2,5 +2,6 @@ def test_with_authenticated_client(client, django_user_model):
     username = "test"
     password = "test"
     django_user_model.objects.create_user(username=username, password=password)
+    client.force_login(user)
     respons = client.get('/')
     assert respons.status_code == 200
