ada643c21071c7aa945d0333a092e4faf121db4b
Rom Tamam <rom.tamam@gmail.com>
2020-12-29 20:23:20 +0200

    flake fixes

--- tests/tests_API_RH.py
+++ tests/tests_API_RH.py
@@ -9,16 +9,26 @@ def test_api_urls():
     """The testing will check if our functions return the required output"""
     # Create soup object & print a status message - 404 not good , 200 good
     soup = API_RH.create_soup(API_RH.create_req())
-    expected_output_urls = ['https://global-redhat.icims.com/jobs/69710/software-development-internship---red-hat-virtualization%2c-student-position/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/80964/senior-devops-engineer---telco-5g-integration/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/83546/senior-product-security-engineer---devsecops-managed-services/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/83285/ci-and-devops-internship/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/69711/software-quality-engineering-internship---local-student-position/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/81168/software-quality-engineer---storage-red-hat-virtualization/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/70227/talent-acquisition-recruiter%2c-engineering-team---12-month-contract/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/83236/principal-software-engineer---object-and-data-services-%28noobaa%29/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/82632/sales-renewals-representative/job?hub=7&in_iframe=1',
-                            'https://global-redhat.icims.com/jobs/81944/senior-technical-writer/job?hub=7&in_iframe=1']
+    expected_output_urls = ['https://global-redhat.icims.com/jobs/69710/software-development-internsh'
+                            'ip---red-hat-virtualization%2c-student-position/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/80964/senior-devops-engineer---telco-'
+                            '5g-integration/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/83546/senior-product-security-enginee'
+                            'r---devsecops-managed-services/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/83285/ci-and-devops-internship/job?hub'
+                            '=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/69711/software-quality-engineering-'
+                            'internship---local-student-position/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/81168/software-quality-engineer---s'
+                            'torage-red-hat-virtualization/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/70227/talent-acquisition-recruiter%'
+                            '2c-engineering-team---12-month-contract/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/83236/principal-software-engineer--'
+                            '-object-and-data-services-%28noobaa%29/job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/82632/sales-renewals-representative/'
+                            'job?hub=7&in_iframe=1',
+                            'https://global-redhat.icims.com/jobs/81944/senior-technical-writer/job?hub'
+                            '=7&in_iframe=1']
     assert API_RH.extract_jobs_url(soup) == expected_output_urls, "The tests for URLs were successful"
 
 
