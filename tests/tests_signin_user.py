da0b7203991c80d3c29f9cb33d8484a7973d22a1
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-01 12:00:06 +0200

    commit for checkout

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -3,5 +3,8 @@ def test_with_authenticated_client(client, django_user_model):
 	password = "test"
 	user = django_user_model.objects.create_user(username=username, password=password)
 	client.force_login(user)
-	respons = client.get('/')
-	assert respons.status_code == 200
+	respons1 = client.get('/sign_in')
+	respons2 = client.get('/personal_area')
+	assert respons1.status_code == 301
+	assert respons2.status_code == 200
+
