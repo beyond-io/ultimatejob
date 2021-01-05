bfd69f997c8bf7a3ad3eab3c392498324245a5d1
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-05 20:14:56 +0200

    a question

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -6,5 +6,5 @@ def test_with_authenticated_client(client, django_user_model):
 	respons1 = client.get('/sign_in')
 	respons2 = client.get('/personal_area')
 	assert respons1.status_code == 301
-	assert respons2.status_code == 200
+	assert respons2.status_code == 301
 
