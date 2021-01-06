3a9c17655b87357c89176fdfc303c72911eed0e9
Rom Tamam <rom.tamam@gmail.com>
2021-01-06 22:04:03 +0200

    cron fix

--- setup.sh
+++ setup.sh
@@ -14,11 +14,10 @@ pipenv sync
 pipenv run python manage.py makemigrations ultimatejobweb
 pipenv run python manage.py migrate
 
-
-# run our app. Nohup and “&” are used to let the setup script finish
 sudo dnf install crontabs
 sudo systemctl start crond
 python manage.py crontab add
+# run our app. Nohup and “&” are used to let the setup script finish
 
 # while our app stays up. The app logs will be collected in nohup.out
 nohup pipenv run python manage.py runserver 0.0.0.0:8000 &
