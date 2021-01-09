947e0856b8e05379ab66eaae72cc7bc7acf70129
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-09 23:06:35 +0200

    flack8

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -2,12 +2,12 @@ from django.test import Client
 
 
 def test_with_authenticated_client(client, django_user_model):
-	username = "test"
-	password = "test"
-	user = django_user_model.objects.create_user(username=username, password=password)
-	client.force_login(user)
-	respons = client.get('/personal_area', follow=True)
-	assert respons.status_code == 200
-	c = Client()
-	respons2 = c.post('/sign_in/', {'username': 'john', 'password': 'smith'})
-	assert respons2.content.find(b"run your job search now") == -1
+    username = "test"
+    password = "test"
+    user = django_user_model.objects.create_user(username=username, password=password)
+    client.force_login(user)
+    respons = client.get('/personal_area', follow=True)
+    assert respons.status_code == 200
+    c = Client()
+    respons2 = c.post('/sign_in/', {'username': 'john', 'password': 'smith'})
+    assert respons2.content.find(b"run your job search now") == -1
