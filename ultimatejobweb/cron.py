41b870459722963eb6370c1d77b2bee9e2f7deff
Rom Tamam <rom.tamam@gmail.com>
2021-01-09 18:18:45 +0200

    cron configuration

--- ultimatejobweb/cron.py
+++ ultimatejobweb/cron.py
@@ -1,5 +1,12 @@
 import os
+import tests.API_FB as fb
+from ultimatejobweb.models import Job
+import time
 
 
 def my_scheduled_job():
     os.system('echo hello >> /vagrant/cron.out')
+    Job.delete_data()
+    time.sleep(3)
+    Job.push_data_api("Facebook", fb.extract_jobs_title(fb.create_soup(fb.create_req())),
+                      fb.extract_jobs_url(fb.create_soup(fb.create_req())))
