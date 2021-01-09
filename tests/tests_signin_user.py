86f4f2def56bf5dd266bb7615fdc352df8be79ed
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-09 21:56:47 +0200

    after yariv notes

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -1,9 +1,13 @@
+from django.test import Client
+
+
 def test_with_authenticated_client(client, django_user_model):
 	username = "test"
 	password = "test"
 	user = django_user_model.objects.create_user(username=username, password=password)
 	client.force_login(user)
-	respons = client.get('/personal_area')
-	assert respons.status_code == 301
-
-
+	respons = client.get('/personal_area', follow=True)
+	assert respons.status_code == 200
+	c = Client()
+	respons2 = c.post('/sign_in/', {'username': 'john', 'password': 'smith'})
+	assert respons2.content.find(b"run your job search now") == -1
