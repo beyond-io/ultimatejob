202af6201b3dcb6223769d2a99c0acc286e2ba64
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-09 13:10:26 +0200

    only login to personal area check

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -3,8 +3,7 @@ def test_with_authenticated_client(client, django_user_model):
 	password = "test"
 	user = django_user_model.objects.create_user(username=username, password=password)
 	client.force_login(user)
-	respons1 = client.get('/sign_in')
-	respons2 = client.get('/personal_area')
-	assert respons1.status_code == 301
-	assert respons2.status_code == 301
+	respons = client.get('/personal_area')
+	assert respons.status_code == 301
+
 
