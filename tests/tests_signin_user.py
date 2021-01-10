ffefb3db657aa00c88f7ffb64ac9bc258bcdfb4e
Bar Ben Naim <ben.nain.bar@gmail.com>
2021-01-10 09:45:27 +0200

    Last time

--- tests/tests_signin_user.py
+++ tests/tests_signin_user.py
@@ -1,6 +1,3 @@
-from django.test import Client
-
-
 def test_with_authenticated_client(client, django_user_model):
     username = "test"
     password = "test"
@@ -8,5 +5,4 @@ def test_with_authenticated_client(client, django_user_model):
     client.force_login(user)
     respons = client.get('/personal_area', follow=True)
     assert respons.status_code == 200
-    respons2 = client.post('/personal_area')
-    assert respons2.content.find(b"run your job search now") == -1
+    assert respons.content.find(b"run your job search now") != -1
